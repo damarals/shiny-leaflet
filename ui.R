322954bdeea4b1b274492b733331e21c86550e05
Daniel Amaral <silva.daniel86@gmail.com>
2020-08-08 02:05:52 -0300

    add api key, created text and sliders inputs

--- ui.R
+++ ui.R
@@ -9,6 +9,21 @@
 
 library(shiny)
 
+geocodeAdddress <- function(address) {
+    require(RJSONIO)
+    source('./key.R')
+    url <- 'http://api.positionstack.com/v1/forward?access_key='
+    url <- URLencode(paste(url, key, '&query=', address, "&limit=1", sep = ""))
+    x <- fromJSON(url, simplify = FALSE)
+    if (length(x$data)) {
+        out <- c(x$data[[1]]$longitude, x$data[[1]]$latitude)
+    } else {
+        out <- NA
+    }
+    out
+}
+
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
@@ -18,11 +33,17 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            textInput("address", "Address to Geolocate", "Example: 1600 Pennsylvania Ave NW, Washington DC"),
+            sliderInput("lat",
+                        "Latitude",
+                        min = -90,
+                        max = 90,
+                        value = 0),
+            sliderInput("long",
+                        "Longitude",
+                        min = -180,
+                        max = 180,
+                        value = 0)
         ),
 
         # Show a plot of the generated distribution
